5b61930b923eafcb09a6e35e3ed0e819a712b181
Ma0de <maode2233@outlook.com>
2025-09-17 23:03:59 +0800

    添加了乱七八糟的文档

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://maode.top/",
+	site: "https://blog.maode.top/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
